d041eb5a91b2ffb13506a55e2acf7452958884aa
AnthonyBasdfg <Anthony@Barrezueta.me>
2025-05-07 00:29:57 -0500

    Cloudflare bugs

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
     unoptimized: true,
   },
   distDir: 'dist',
+  trailingSlash: true,  // Mejora la compatibilidad con hosting estático
+  // Garantiza que las rutas sean compatibles con hosting estático
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
 }
 
 module.exports = nextConfig
