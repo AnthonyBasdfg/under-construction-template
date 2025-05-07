3eecde5601430d29b7cbd156c5e2524bfe7be37f
AnthonyBasdfg <Anthony@Barrezueta.me>
2025-05-07 00:36:44 -0500

    3

--- .wrangler/tmp/bundle-dgx32j/middleware-insertion-facade.js
+++
@@ -1,4 +0,0 @@
-				import { __facade_registerInternal__ } from "C:\\Users\\User0\\AppData\\Local\\npm-cache\\_npx\\32026684e21afda6\\node_modules\\wrangler\\templates\\middleware\\loader-sw.ts";
-				import * as __MIDDLEWARE_0__ from "C:\\Users\\User0\\AppData\\Local\\npm-cache\\_npx\\32026684e21afda6\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
-import * as __MIDDLEWARE_1__ from "C:\\Users\\User0\\AppData\\Local\\npm-cache\\_npx\\32026684e21afda6\\node_modules\\wrangler\\templates\\middleware\\middleware-miniflare3-json-error.ts";
-				__facade_registerInternal__([__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default])
